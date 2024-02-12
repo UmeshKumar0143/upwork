512b4a16ecd6448f961099bb9ebcf94cd9ccdc01
Santa116 <santabaner1223@gmail.com>
2024-02-13 02:09:37 +0900

    tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,32 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      // Add custom utilities
+      lineClamp: {
+        3: {
+          overflow: 'hidden',
+          display: '-webkit-box',
+          '-webkit-box-orient': 'vertical',
+          '-webkit-line-clamp': '3', // Number of lines you want to clamp
+          textOverflow: 'ellipsis',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // Plugin to add utilities to Tailwind
+    function({ addUtilities }) {
+      addUtilities({
+        '.line-clamp-3': {
+          overflow: 'hidden',
+          display: '-webkit-box',
+          '-webkit-box-orient': 'vertical',
+          '-webkit-line-clamp': '3', // Adjust the number for different line clamps
+          textOverflow: 'ellipsis',
+        },
+      })
+    },
+  ],
 }
 
